37d566361490e889c7cf1c0fee2e94d793512de9
Philippe Rougerie <philippe.rougerie@etu.uca.fr>
2019-02-06 13:15:36 +0100

    Ajout de la documentaion.

---
+++ ListAlbum/PROUGERIE_HSOEUR.ListeAlbum/WPFApp/views/Modifmage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFApp.views
+{
+    /// <summary>
+    /// Logique d'interaction pour Modifmage.xaml
+    /// </summary>
+    public partial class Modifmage : Window
+    {
+        //Initialise une fenetre 
+        public Modifmage()
+        {
+            InitializeComponent();
+        }
+        // Verifie sur l'uri entrée est valide et affiche l'image si c'est le cas
+        private void ImageVerified(object sender, RoutedEventArgs e)
+        {
+            
+            Uri uri;
+            if (Uri.TryCreate(TexBoxImage.Text, UriKind.Absolute, out uri) && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps || uri.Scheme == Uri.UriSchemeFile))
+            {
+                try
+                {
+                    Imageverif.Source = new BitmapImage(uri);
+                }
+                catch (DirectoryNotFoundException dirEx)
+                {
+                    TexBoxImage.Text = "Directory not found: " + dirEx.Message;
+                    Imageverif.Source = new BitmapImage(new Uri("Covers/CoverDefault.jpg", UriKind.Relative));
+                }
+            }
+
+
+        }
+        //Ferme la fenetre et ajoute l'image si les données sont correctes
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                Close();
+        }
+        private void Image_Choiced(object sender, RoutedEventArgs e)
+        {
+            string filepath = "";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JPG files (*.jpg)|*.jpg|PNG files (*.png)|*.png|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filepath = openFileDialog.InitialDirectory + openFileDialog.FileName;
+            }
+            TexBoxImage.Text = filepath;
+        }
+    }
+}
---
+++ ListAlbum/README.md
@@ -0,0 +1,15 @@
+#Gestion d'albums:
+Cette application permet de gerer une liste d'albums en effectuant plusieurs modifications.
+Parmis ces fonctionnalités on a la supprimation d'un album, la sauvegarde, l'ajout de morceau, la lecture de morceaux...
+
+#Ce projet est effectué dans le cadre du cours de conception orientée objet en première année de DUT informatique de l'UCA.
+
+
+#La partie documentation est dans le sous-dossier Documentation de ListAlbum
+
+#La solution se trouve dans le dossier PROUGERIE_HSOEUR.ListeAlbum.
+Le programme doit s'executer une première fois en administrateur depuis l'executable du dossier. Il peut être ensuite lancé depuis le raccourci.
+Tous les formats audio de la lecture fonctionnent, sauf le flac dont le temps qui ne s'affiche pas.
+Le fichier audio pour Karma Police est fourni pour pouvoir tester le lecteur (mais ne fonctionne pas pour l'installation).
+
+#Ce projet est un travail en binome réalisé par Hugo SOEUR et Philippe Rougerie.
